f83add68d080f5511eae66e4b7db6721621ee801
Ana Negre <98236062+anita476@users.noreply.github.com>
2026-03-26 19:05:09 -0300

    Add clang formatting

--- source/drivers/MCAL/include/pisr.h
+++ source/drivers/MCAL/include/pisr.h
@@ -1,8 +1,8 @@
-/***************************************************************************//**
-  @file     pisr.h
-  @brief    Periodic Interrupt (PISR) driver
-  @author   Nicolás Magliola
- ******************************************************************************/
+/***************************************************************************/ /**
+   @file     pisr.h
+   @brief    Periodic Interrupt (PISR) driver
+   @author   Nicolás Magliola
+  ******************************************************************************/
 
 #ifndef _PISR_H_
 #define _PISR_H_
@@ -14,22 +14,19 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
 
 #define PISR_FREQUENCY_HZ 1000U
 
-#define PISR_CANT        8
-
+#define PISR_CANT 8
 
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
  ******************************************************************************/
 
-typedef void (*pisr_callback_t) (void);
-
+typedef void (*pisr_callback_t)(void);
 
 /*******************************************************************************
  * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
@@ -45,8 +42,7 @@ typedef void (*pisr_callback_t) (void);
  * @param period PISR period in ticks
  * @return Registration succeed
  */
-bool pisrRegister (pisr_callback_t fun, unsigned int period);
-
+bool pisrRegister(pisr_callback_t fun, unsigned int period);
 
 /*******************************************************************************
  ******************************************************************************/
--- source/drivers/MCAL/pisr.c
+++ source/drivers/MCAL/pisr.c
@@ -12,48 +12,46 @@ static bool active; // evaluate if systick was enabled
 static unsigned int ticks;
 
 typedef struct {
-  unsigned int period;
-  pisr_callback_t callback;
+	unsigned int period;
+	pisr_callback_t callback;
 } callback;
 
 typedef struct {
-  callback callbacks[PISR_CANT];
-  int used_irqs;
-  int max_period;
+	callback callbacks[PISR_CANT];
+	int used_irqs;
+	int max_period;
 } pisrType;
 static pisrType pIrqs;
 
 bool pisrRegister(pisr_callback_t fun, unsigned int period) {
-  if (pIrqs.used_irqs >= PISR_CANT) {
-    return false;
-  }
-  if (!active) {
-    // if it wasnt, activate
-    SysTick->CTRL = 0x00; // reset everything
-    SysTick->LOAD = (TICK_MS * PISR_FREQUENCY_HZ * 100) -
-                    1;   // 12499999L; // <= 125 ms @ 100Mhz
-    SysTick->VAL = 0x00; // current value
-    SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk |
-                    SysTick_CTRL_ENABLE_Msk;
-    active = true;
-  }
-  pIrqs.callbacks[pIrqs.used_irqs].callback = fun;
-  pIrqs.callbacks[pIrqs.used_irqs].period = period;
-  if (period > pIrqs.max_period) {
-    pIrqs.max_period = period;
-  }
-  pIrqs.used_irqs++;
-  return true;
+	if (pIrqs.used_irqs >= PISR_CANT) {
+		return false;
+	}
+	if (!active) {
+		// if it wasnt, activate
+		SysTick->CTRL = 0x00;									 // reset everything
+		SysTick->LOAD = (TICK_MS * PISR_FREQUENCY_HZ * 100) - 1; // 12499999L; // <= 125 ms @ 100Mhz
+		SysTick->VAL = 0x00;									 // current value
+		SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
+		active = true;
+	}
+	pIrqs.callbacks[pIrqs.used_irqs].callback = fun;
+	pIrqs.callbacks[pIrqs.used_irqs].period = period;
+	if (period > pIrqs.max_period) {
+		pIrqs.max_period = period;
+	}
+	pIrqs.used_irqs++;
+	return true;
 }
 
 void SysTick_Handler(void) {
-  ticks++;
-  for (int i = 0; i < pIrqs.used_irqs; i++) {
-    if (ticks % pIrqs.callbacks[i].period == 0) {
-      pIrqs.callbacks[i].callback();
-    }
-  }
-  if (ticks == pIrqs.max_period) {
-    ticks = 0;
-  }
+	ticks++;
+	for (int i = 0; i < pIrqs.used_irqs; i++) {
+		if (ticks % pIrqs.callbacks[i].period == 0) {
+			pIrqs.callbacks[i].callback();
+		}
+	}
+	if (ticks == pIrqs.max_period) {
+		ticks = 0;
+	}
 }
